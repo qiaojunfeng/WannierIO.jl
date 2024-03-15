8ee200763c4bcb7e6150b03dff2e05a0fa9975e0
Junfeng Qiao <qiaojunfeng@outlook.com>
2024-03-15 16:23:50 +0100

    Add read/write `u.mat` and `u_dis.mat` files

---
+++ test/w90/u_mat.jl
@@ -0,0 +1,31 @@
+@testitem "read/write u.mat" begin
+    using LazyArtifacts
+    chk = read_chk(artifact"Si2/reference/Si2.chk.fmt")
+
+    Udismat = WannierIO.read_u_mat(artifact"Si2/reference/Si2_u.mat")
+    @assert Udismat.U ≈ chk.Uml
+    @assert Udismat.kpoints ≈ chk.kpoints
+
+    tmpfile = tempname(; cleanup=true)
+    WannierIO.write_u_mat(tmpfile, chk.Uml, chk.kpoints)
+    Udismat2 = WannierIO.read_u_mat(tmpfile)
+    @assert Udismat2.U ≈ chk.Uml
+    @assert Udismat2.kpoints ≈ chk.kpoints
+end
+
+@testitem "read/write u_dis.mat" begin
+    using LazyArtifacts
+    chk = read_chk(artifact"Si2/reference/Si2.chk.fmt")
+
+    Udismat = WannierIO.read_u_mat(artifact"Si2/reference/Si2_u_dis.mat")
+    # do not use `get_Udis` since it sorts the band indices, here we want to
+    # compare the raw data
+    @assert Udismat.U ≈ chk.Udis
+    @assert Udismat.kpoints ≈ chk.kpoints
+
+    tmpfile = tempname(; cleanup=true)
+    WannierIO.write_u_mat(tmpfile, chk.Udis, chk.kpoints)
+    Udismat2 = WannierIO.read_u_mat(tmpfile)
+    @assert Udismat2.U ≈ chk.Udis
+    @assert Udismat2.kpoints ≈ chk.kpoints
+end
