e374c37993f76b1c091138bc1ecaa43e31913ec4
Junfeng Qiao <qiaojunfeng@outlook.com>
2025-05-16 15:45:32 +0200

    Add test for `vec3` `mat3`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,6 @@
-# using Aqua
-# only test ambiguities in current module, otherwise fails due to ambiguities in other packages
-#   https://github.com/JuliaTesting/Aqua.jl/issues/77
-# disable project_extras since we don't use julia < 1.2
-# Aqua.test_all(WannierIO; ambiguities=false, project_extras=false)
-# Aqua.test_ambiguities(WannierIO)
-
 using TestItemRunner
 
-@run_package_tests verbose = true
+# Filter tests
+# @run_package_tests verbose=true filter=ti->endswith(ti.filename, "type.jl")
+
+@run_package_tests verbose=true
