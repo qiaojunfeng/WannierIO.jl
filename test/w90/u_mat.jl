39c580ec52f5f9e641211f6db5175ec5b5e00e05
Junfeng Qiao <qiaojunfeng@outlook.com>
2026-02-08 19:10:45 +0100

    Fix u_mat test

--- test/w90/u_mat.jl
+++ test/w90/u_mat.jl
@@ -3,14 +3,14 @@
     chk = read_chk(artifact"Si2/outputs/Si2.chk.fmt")
 
     Udismat = WannierIO.read_u_mat(artifact"Si2/outputs/Si2_u.mat")
-    @assert Udismat.U ≈ chk.Uml
-    @assert Udismat.kpoints ≈ chk.kpoints
+    @test Udismat.U ≈ chk.Uml
+    @test Udismat.kpoints ≈ chk.kpoints
 
     tmpfile = tempname(; cleanup=true)
     WannierIO.write_u_mat(tmpfile, chk.Uml, chk.kpoints)
     Udismat2 = WannierIO.read_u_mat(tmpfile)
-    @assert Udismat2.U ≈ chk.Uml
-    @assert Udismat2.kpoints ≈ chk.kpoints
+    @test Udismat2.U ≈ chk.Uml
+    @test Udismat2.kpoints ≈ chk.kpoints
 end
 
 @testitem "read/write u_dis.mat" begin
@@ -20,12 +20,12 @@ end
     Udismat = WannierIO.read_u_mat(artifact"Si2/outputs/Si2_u_dis.mat")
     # do not use `get_Udis` since it sorts the band indices, here we want to
     # compare the raw data
-    @assert Udismat.U ≈ chk.Udis
-    @assert Udismat.kpoints ≈ chk.kpoints
+    @test Udismat.U ≈ chk.Udis
+    @test Udismat.kpoints ≈ chk.kpoints
 
     tmpfile = tempname(; cleanup=true)
     WannierIO.write_u_mat(tmpfile, chk.Udis, chk.kpoints)
     Udismat2 = WannierIO.read_u_mat(tmpfile)
-    @assert Udismat2.U ≈ chk.Udis
-    @assert Udismat2.kpoints ≈ chk.kpoints
+    @test Udismat2.U ≈ chk.Udis
+    @test Udismat2.kpoints ≈ chk.kpoints
 end
