b908b9d545980a8332138b02f19eeda78b73206a
Junfeng Qiao <qiaojunfeng@outlook.com>
2026-03-10 22:18:06 +0100

    Add AGENTS.md

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,28 @@
 using TestItemRunner
-
-# Filter tests
-# @run_package_tests verbose=true filter=ti->endswith(ti.filename, "type.jl")
+using WannierIO
+using Documenter
 
 # Use local artifacts, if they are changed locally.
 # macro artifact_str(path)
 #     joinpath(Sys.homedir(), "git/WannierDatasets/datasets", path)
 # end
 
-@run_package_tests verbose=true
+# Get filter string from command line arguments if provided
+# Usage: julia --project test/runtests.jl "Name of Test"
+filter_name = length(ARGS) > 0 ? ARGS[1] : nothing
+
+if isnothing(filter_name)
+    println("Running all tests...")
+
+    DocMeta.setdocmeta!(WannierIO, :DocTestSetup, :(using WannierIO); recursive=true)
+    doctest(
+        WannierIO;
+        # fix=true,  # update all the output in `jldoctest`
+    )
+
+    @run_package_tests verbose = true
+else
+    println("Running specific test: $filter_name")
+
+    @run_package_tests verbose = true filter = ti -> endswith(ti.filename, filter_name)
+end
